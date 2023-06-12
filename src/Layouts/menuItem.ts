32f184382297244da14edcffe99facff92dad681
sr <7518647+wendychaung@users.noreply.github.com>
2023-06-12 20:28:09 +0800

    Update menuItem.ts

--- src/Layouts/menuItem.ts
+++ src/Layouts/menuItem.ts
@@ -3,22 +3,27 @@ import { MenuItemType } from '@paljs/ui/types';
 const items: MenuItemType[] = [
   {
     title: 'menus.Proposal',
+    icon: { name: 'browser-outline' },
     link: { href: '/proposal' },
   },
   {
     title: 'menus.Project',
+    icon: { name: 'layout-outline' },
     link: { href: '/project' },
   },
   {
     title: 'menus.Guild',
+    icon: { name: 'people-outline' },
     link: { href: '/guild' },
   },
   {
     title: 'menus.city-hall',
+    icon: { name: 'shield-outline' },
     link: { href: '/city-hall' },
   },
   {
     title: 'menus.assets',
+    icon: { name: 'cube-outline' },
     link: { href: '/assets' },
   },
 ];
