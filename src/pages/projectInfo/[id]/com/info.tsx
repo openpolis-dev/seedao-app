38c72b879b6f49e1567792603c5ff43dfd3b9ce4
sr <7518647+wendychaung@users.noreply.github.com>
2023-06-12 16:53:18 +0800

    add project Info

---
+++ src/pages/projectInfo/[id]/com/info.tsx
@@ -0,0 +1,86 @@
+import Container from '@paljs/ui/Container';
+import styled from 'styled-components';
+import { Button } from '@paljs/ui/Button';
+
+const Box = styled.div`
+  margin-top: 40px;
+`;
+
+const TopImg = styled.div``;
+
+const InfoBox = styled.div`
+  dl {
+    margin-bottom: 10px;
+    display: flex;
+    align-items: flex-start;
+  }
+  .info {
+    margin-right: 10px;
+  }
+  dt {
+    line-height: 1.5em;
+    display: inline-block;
+    min-width: 140px;
+    background: #f5f5f5;
+    padding: 0 20px;
+  }
+  dd {
+    display: flex;
+    align-items: center;
+    margin-bottom: 10px;
+  }
+`;
+
+const Title = styled.div`
+  font-weight: bold;
+`;
+export default function Info() {
+  return (
+    <Box>
+      <Container>
+        <TopImg>
+          <img
+            src="https://img0.baidu.com/it/u=2050198963,701666245&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=501"
+            alt=""
+          />
+        </TopImg>
+        <InfoBox>
+          <dl>
+            <dt>Project Name:</dt>
+            <dd>
+              <div className="info">全球DAO场战略项目</div>
+              <Button shape="Rectangle" appearance="outline" size="Tiny">
+                关闭项目
+              </Button>
+            </dd>
+          </dl>
+          <Title>预算</Title>
+          <dl>
+            <dt>积分:</dt>
+            <dd>
+              <div className="info">
+                <span>1000</span>
+                <span>（已使用100，剩余900）</span>
+              </div>
+              <Button shape="Rectangle" appearance="outline" size="Tiny">
+                修改
+              </Button>
+            </dd>
+          </dl>
+          <dl>
+            <dt>USDT:</dt>
+            <dd>
+              <div className="info">
+                <span>1000</span>
+                <span>（已使用100，剩余900）</span>
+              </div>
+              <Button shape="Rectangle" appearance="outline" size="Tiny">
+                修改
+              </Button>
+            </dd>
+          </dl>
+        </InfoBox>
+      </Container>
+    </Box>
+  );
+}
---
+++ src/pages/projectInfo/[id]/index.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import Layout from 'Layouts';
+import Row from '@paljs/ui/Row';
+import Col from '@paljs/ui/Col';
+import { Tabs, Tab } from '@paljs/ui/Tabs';
+import styled from 'styled-components';
+import { useRouter } from 'next/router';
+import { Card, CardHeader } from '@paljs/ui/Card';
+import Info from 'pages/projectInfo/[id]/com/info';
+
+const Box = styled.div`
+  position: relative;
+  .tab-content {
+    padding: 0 !important;
+  }
+`;
+export default function Index() {
+  const router = useRouter();
+  return (
+    <Layout title="SeeDAO Project">
+      <Card>
+        <Box>
+          <Row>
+            <Col breakPoint={{ xs: 12 }}>
+              <CardHeader>
+                <Tabs>
+                  <Tab key="0" title="Project Info" responsive>
+                    <Info />
+                  </Tab>
+                  <Tab key="1" title="Members" responsive>
+                    <div>ddd</div>
+                  </Tab>
+                  <Tab key="2" title="Project Assets" responsive>
+                    <div>ddd</div>
+                  </Tab>
+                  <Tab key="3" title="Project Proposal" responsive>
+                    <div>ddd</div>
+                  </Tab>
+                </Tabs>
+              </CardHeader>
+            </Col>
+          </Row>
+        </Box>
+      </Card>
+    </Layout>
+  );
+}
