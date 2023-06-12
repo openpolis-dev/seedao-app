d47cb2a2dca50295ff5dabf70a035d5ca4b60d4c
sr <7518647+wendychaung@users.noreply.github.com>
2023-06-12 16:44:34 +0800

    update project

--- src/pages/project/index.tsx
+++ src/pages/project/index.tsx
@@ -1,6 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Layout from 'Layouts';
+import Row from '@paljs/ui/Row';
+import Col from '@paljs/ui/Col';
+import { Tabs, Tab } from '@paljs/ui/Tabs';
+import ProjectAllList from 'pages/project/com/list';
+import styled from 'styled-components';
+import { ButtonLink } from '@paljs/ui/Button';
+import { useRouter } from 'next/router';
+import { Card, CardHeader } from '@paljs/ui/Card';
+
+const Box = styled.div`
+  position: relative;
+  .tab-content {
+    padding: 0 !important;
+  }
+`;
+
+const TopLine = styled.div`
+  position: absolute;
+  right: 20px;
+  top: 14px;
+  z-index: 9;
+  cursor: pointer;
+`;
 
 export default function Index() {
-  return <Layout title="SeeDAO Project">project</Layout>;
+  const router = useRouter();
+  const [current, setCurrent] = useState<number>(0);
+  const [list] = useState([
+    {
+      name: 'ALL',
+      id: 0,
+    },
+    {
+      name: 'Closed',
+      id: 1,
+    },
+    {
+      name: 'Joined',
+      id: 2,
+    },
+  ]);
+
+  const selectCurrent = (e: number) => {
+    setCurrent(e);
+    console.log(current);
+  };
+
+  return (
+    <Layout title="SeeDAO Project">
+      <Card>
+        <Box>
+          <TopLine>
+            <ButtonLink onClick={() => router.push('/')} fullWidth shape="Rectangle">
+              Create Project
+            </ButtonLink>
+          </TopLine>
+          <Row>
+            <Col breakPoint={{ xs: 12 }}>
+              <CardHeader>
+                <Tabs activeIndex={0} onSelect={(e) => selectCurrent(e)}>
+                  {list.map((item) => (
+                    <Tab key={item.id} title={item.name} responsive>
+                      <ProjectAllList />
+                    </Tab>
+                  ))}
+                </Tabs>
+              </CardHeader>
+            </Col>
+          </Row>
+        </Box>
+      </Card>
+    </Layout>
+  );
 }
