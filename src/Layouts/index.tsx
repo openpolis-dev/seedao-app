a1b6673eb0d4c80e44da8bb7a16116e0014072c0
sr <7518647+wendychaung@users.noreply.github.com>
2023-06-13 15:01:57 +0800

    Update index.tsx

--- src/Layouts/index.tsx
+++ src/Layouts/index.tsx
@@ -81,21 +81,21 @@ const LayoutPage: React.FC<SEOProps> = ({ children, ...rest }) => {
                 responsive
                 className="menu-sidebar"
               >
-                {seeHeader && (
-                  <header>
-                    <Button
-                      size="Tiny"
-                      status="Primary"
-                      onClick={() => {
-                        setMenuState(!menuState);
-                        menuRef.current?.toggle();
-                      }}
-                      fullWidth
-                    >
-                      {menuState ? <EvaIcon name="arrow-circle-up" /> : <EvaIcon name="arrow-circle-down" />}
-                    </Button>
-                  </header>
-                )}
+                {/*{seeHeader && (*/}
+                {/*  <header>*/}
+                {/*    <Button*/}
+                {/*      size="Tiny"*/}
+                {/*      status="Primary"*/}
+                {/*      onClick={() => {*/}
+                {/*        setMenuState(!menuState);*/}
+                {/*        menuRef.current?.toggle();*/}
+                {/*      }}*/}
+                {/*      fullWidth*/}
+                {/*    >*/}
+                {/*      {menuState ? <EvaIcon name="arrow-circle-up" /> : <EvaIcon name="arrow-circle-down" />}*/}
+                {/*    </Button>*/}
+                {/*  </header>*/}
+                {/*)}*/}
                 <SidebarBody>
                   <Menu
                     nextJs
