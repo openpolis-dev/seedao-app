58e8b00e698e619ee29ff8ce51f3ec81671af44c
sr <7518647+wendychaung@users.noreply.github.com>
2023-06-13 23:02:12 +0800

    update github yml

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 module.exports = {
-    i18n: {
-      // These are all the locales you want to support in
-      // your application
-      locales: ['en', "zh"],
-      // This is the default locale you want to be used when visiting
-      // a non-locale prefixed path e.g. `/hello`
-      defaultLocale: 'en',
-    },
-  }
+  distDir: 'build',
+  trailingSlash: true,
+  i18n: {
+    // These are all the locales you want to support in
+    // your application
+    locales: ['en', 'zh'],
+    // This is the default locale you want to be used when visiting
+    // a non-locale prefixed path e.g. `/hello`
+    defaultLocale: 'en',
+  },
+};
