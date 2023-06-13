49522f5e5002d69d1ac546178f9e96a9fc185730
sr <7518647+wendychaung@users.noreply.github.com>
2023-06-14 00:51:02 +0800

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,12 @@
+const isProd = process.env.NODE_ENV === 'production';
+module.exports = {
+  reactStrictMode: true,
+  // swcMinify: false,
+  trailingSlash: true,
+  experimental: {
+    // ssr and displayName are configured by default
+    styledComponents: true,
+  },
+
+  assetPrefix: isProd ? '/os-frondend-preview' : '',
+};
