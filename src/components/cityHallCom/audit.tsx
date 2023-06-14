0432582009301396ce976e40c1f0c2f2f947a1ad
sr <7518647+wendychaung@users.noreply.github.com>
2023-06-14 16:39:34 +0800

    update city hall

---
+++ src/components/cityHallCom/audit.tsx
@@ -0,0 +1,183 @@
+import styled from 'styled-components';
+import { Button } from '@paljs/ui/Button';
+import Select from '@paljs/ui/Select';
+import React, { useState } from 'react';
+import Page from 'components/pagination';
+import DatePickerStyle from 'components/datePicker';
+
+const Box = styled.div``;
+const FirstLine = styled.div`
+  display: flex;
+  //flex-direction: column;
+  margin: 40px 0 20px;
+  align-items: center;
+  flex-wrap: wrap;
+
+  //justify-content: space-between;
+`;
+
+const TopLine = styled.ul`
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+
+  li {
+    display: flex;
+    align-items: center;
+    margin-right: 40px;
+
+    .tit {
+      padding-right: 20px;
+    }
+
+    .sel {
+      min-width: 150px;
+    }
+  }
+`;
+
+const TimeLine = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+
+const TimeBox = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: 20px;
+`;
+
+const BorderBox = styled.div`
+  border: 1px solid #eee;
+  padding: 10px 20px;
+  border-radius: 5px;
+`;
+
+const MidBox = styled.div`
+  margin: 0 20px;
+`;
+
+const TopBox = styled.div`
+  background: #f5f5f5;
+  display: flex;
+  justify-content: flex-start;
+  padding: 10px;
+  margin: 0 0 30px;
+  button {
+    margin-left: 20px;
+  }
+`;
+
+export default function Audit() {
+  const [page, setPage] = useState(1);
+  const [pageSize, setPageSize] = useState(10);
+  const [total, setTotal] = useState(100);
+  const [show, setShow] = useState(false);
+  const [dateTime, setDateTime] = useState<Date | null>(null);
+
+  const statusOption: { value: any; label: any }[] = [
+    { label: '待审核', value: '待审核' },
+    { value: '被驳回', label: '被驳回' },
+    { value: '待发放', label: '待发放' },
+    { value: '已发放', label: '已发放' },
+  ];
+
+  const handlePage = (num: number) => {
+    setPage(num + 1);
+  };
+  const handlePageSize = (num: number) => {
+    setPageSize(num);
+  };
+  const handleShow = (num: number) => {
+    setShow(true);
+    console.log(num);
+  };
+  const closeShow = () => {
+    setShow(false);
+  };
+
+  const changeDate = (time: Date) => {
+    console.log(time?.getTime());
+    const str = new Date(time?.getTime());
+    setDateTime(str);
+  };
+  return (
+    <Box>
+      <FirstLine>
+        <TopLine>
+          <li>
+            <span className="tit">状态</span>
+            <Select className="sel" options={statusOption} placeholder="Status" />
+          </li>
+          <li>
+            <span className="tit">治理公会</span>
+            <Select className="sel" options={statusOption} placeholder="Status" />
+          </li>
+          <li>
+            <span className="tit">操作人</span>
+            <Select className="sel" options={statusOption} placeholder="Status" />
+          </li>
+        </TopLine>
+        <TimeLine>
+          <TimeBox>
+            <BorderBox>
+              <DatePickerStyle placeholder="开始时间" onChange={changeDate} dateTime={dateTime} />
+            </BorderBox>
+            <MidBox>~</MidBox>
+            <BorderBox>
+              <DatePickerStyle placeholder="开始时间" onChange={changeDate} dateTime={dateTime} />
+            </BorderBox>
+          </TimeBox>
+          <Button size="Medium">导出</Button>
+        </TimeLine>
+      </FirstLine>
+      <TopBox>
+        <Button>通过</Button>
+        <Button appearance="outline">驳回</Button>
+      </TopBox>
+      <table className="table" cellPadding="0" cellSpacing="0">
+        <tr>
+          <th>时间</th>
+          <th>钱包地址</th>
+          <th>登记积分</th>
+          <th>登记Token</th>
+          <th>事项内容</th>
+          <th>备注</th>
+          <th>状态</th>
+          <th>登记人</th>
+          <th>审核人</th>
+        </tr>
+        <tr>
+          <td>2023/06/13</td>
+          <td>0Xfds...sdf</td>
+          <td>d</td>
+          <td>100USD</td>
+          <td>酬劳</td>
+          <td>--</td>
+          <td>待审核</td>
+          <td>WD</td>
+          <td>WD</td>
+        </tr>
+        <tr>
+          <td>2023/06/13</td>
+          <td>0Xfds...sdf</td>
+          <td>d</td>
+          <td>100USD</td>
+          <td>酬劳</td>
+          <td>--</td>
+          <td>待审核</td>
+          <td>WD</td>
+          <td>WD</td>
+        </tr>
+      </table>
+      <Page
+        itemsPerPage={pageSize}
+        total={total}
+        current={page - 1}
+        handleToPage={handlePage}
+        handlePageSize={handlePageSize}
+      />
+    </Box>
+  );
+}
--- src/pages/city-hall/index.tsx
+++ src/pages/city-hall/index.tsx
@@ -1,6 +1,50 @@
 import React from 'react';
 import Layout from 'Layouts';
+import { Card, CardHeader, CardBody } from '@paljs/ui/Card';
+import styled from 'styled-components';
+import Row from '@paljs/ui/Row';
+import Col from '@paljs/ui/Col';
+import { Tab, Tabs } from '@paljs/ui/Tabs';
+import Audit from 'components/cityHallCom/audit';
+import { Button } from '@paljs/ui/Button';
 
+const Box = styled.div`
+  position: relative;
+  .tab-content {
+    padding: 0 !important;
+  }
+`;
+const CardBox = styled(Card)`
+  min-height: 85vh;
+`;
+
+const TopBox = styled.div`
+  padding: 20px;
+`;
 export default function Index() {
-  return <Layout title="SeeDAO City Hall">city hall</Layout>;
+  return (
+    <Layout title="SeeDAO City Hall">
+      <CardBox>
+        <Box>
+          <Row>
+            <Col breakPoint={{ xs: 12 }}>
+              <TopBox>
+                <Tabs>
+                  <Tab key="0" title="积分及token审核" responsive>
+                    <Audit />
+                  </Tab>
+                  <Tab key="1" title="项目审核" responsive>
+                    <div>ddd</div>
+                  </Tab>
+                  <Tab key="2" title="发放" responsive>
+                    <div>ddd</div>
+                  </Tab>
+                </Tabs>
+              </TopBox>
+            </Col>
+          </Row>
+        </Box>
+      </CardBox>
+    </Layout>
+  );
 }
