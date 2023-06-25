059fdc5a149c3c0c53a1917a4c5c1f9223ca1ce9
CatFlow <22841916+Meow711@users.noreply.github.com>
2023-06-26 02:04:15 +0800

    test script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,15 +4,17 @@ import AuthProvider from 'providers/authProvider';
 import React from 'react';
 import 'styles/quill.css';
 import Script from 'next/script';
+import { useRouter } from 'next/router';
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface CustomPageProps {}
 
 export default function App({ Component, pageProps }: AppProps<CustomPageProps>) {
   const AnyComponent = Component as any;
+  const router = useRouter();
 
   return (
     <>
-      <Script src="sdnChatWidget.js" type="text/javascript" />
+      <Script src={`${router.basePath}/sdnChatWidget.js`} type="text/javascript" />
       <AuthProvider>
         <Web3Provider>
           <AnyComponent {...pageProps} />
