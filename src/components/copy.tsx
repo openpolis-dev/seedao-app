c244825e38974fae383dc965ea203b7b86c9df97
CatFlow <22841916+Meow711@users.noreply.github.com>
2023-07-06 01:31:22 +0800

    component: modify the copy box

--- src/components/copy.tsx
+++ src/components/copy.tsx
@@ -1,24 +1,34 @@
-import React, { useRef } from 'react';
+import useTranslation from 'hooks/useTranslation';
+import React, { useState } from 'react';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import styled from 'styled-components';
 
 interface ICopyProps {
-  children: React.ReactElement;
+  children: React.ReactNode;
   text: string;
   onCopy?: (text: string, result: boolean) => void;
 }
 
-const CopyBox: React.FC<ICopyProps> = ({ children, text, onCopy }) => {
-  const target = useRef(null);
+const CopyBox: React.FC<ICopyProps> = ({ children, text }) => {
+  const { t } = useTranslation();
+  const [isCopied, setIsCopied] = useState(false);
 
   const handleCopy = (text: string, result: any) => {
-    onCopy && onCopy(text, result);
+    if (result) {
+      setIsCopied(true);
+      setTimeout(() => {
+        setIsCopied(false);
+      }, 2000);
+    }
   };
 
   return (
     <>
       <CopyToClipboard text={text} onCopy={handleCopy}>
-        <CopyContent ref={target}>{children}</CopyContent>
+        <CopyContent>
+          {children}
+          {isCopied && <span className="tooltip-content">{t('general.Copied')}</span>}
+        </CopyContent>
       </CopyToClipboard>
     </>
   );
@@ -28,4 +38,27 @@ export default CopyBox;
 
 const CopyContent = styled.div`
   cursor: pointer;
+  position: relative;
+  .tooltip-content {
+    position: absolute;
+    padding: 5px 12px;
+    border-radius: 8px;
+    transition: opacity 0.3s ease, visibility 0.3s ease;
+    right: 30px;
+    top: -5px;
+    white-space: nowrap;
+    background: #000;
+    color: #fff;
+    z-index: 99;
+    font-size: 12px;
+  }
+  .tooltip-content::before {
+    content: '';
+    position: absolute;
+    border: 6px solid transparent;
+    border-bottom-color: #000;
+    top: 10px;
+    right: -6px;
+    transform: translateX(50%) rotate(90deg);
+  }
 `;
