9c75b7c635d461678449e0ecb6f4d496b1df1858
Frozen <1884084+xrdavies@users.noreply.github.com>
2023-07-06 17:18:52 +0800

    update path

--- next.config.js
+++ next.config.js
@@ -10,12 +10,12 @@ module.exports = {
   images: {
     unoptimized: true,
   },
-  assetPrefix: isProd ? '/os-frontend-preview/' : '',
-  basePath: isProd ? '/os-frontend-preview' : '',
-  images: {
-    loader: 'akamai',
-    path: isProd ? '/os-frontend-preview/' : '/',
-  },
+  // assetPrefix: isProd ? '/os-frontend-preview/' : '',
+  // basePath: isProd ? '/os-frontend-preview' : '',
+  // images: {
+  //   loader: 'akamai',
+  //   path: isProd ? '/os-frontend-preview/' : '/',
+  // },
 };
 
 // please refer https://github.com/gregrickaby/nextjs-github-pages for github pages deployment
