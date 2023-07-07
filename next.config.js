a876fba4c80cea4082db0fffa28808a7350f8d1e
Frozen <1884084+xrdavies@users.noreply.github.com>
2023-07-08 04:14:58 +0800

    fix build error

--- next.config.js
+++ next.config.js
@@ -16,6 +16,10 @@ module.exports = {
     loader: 'akamai',
     path: '/',
   },
+  typescript: {
+    ignoreBuildErrors: true
+  }
 };
 
 // please refer https://github.com/gregrickaby/nextjs-github-pages for github pages deployment
+// github page issue https://medium.com/geekculture/github-pages-with-dynamic-routes-40f512900efa
