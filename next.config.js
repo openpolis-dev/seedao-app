5469bceea7621b64873aff6c03b81dff42e4ed2d
Frozen <1884084+xrdavies@users.noreply.github.com>
2023-07-08 04:21:50 +0800

    update basePath

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ module.exports = {
   //   unoptimized: true,
   // },
   // assetPrefix: isProd ? '/os-frontend-preview/' : '',
-  // basePath: isProd ? '/os-frontend-preview' : '',
+  basePath: '',
   images: {
     loader: 'akamai',
     path: '/',
