2f492a8aa2253a75037bd881330fef1a6d349df0
CatFlow <22841916+Meow711@users.noreply.github.com>
2023-07-10 16:19:48 +0800

    remove useless code

--- src/hooks/useRequests.tsx
+++
@@ -1,8 +0,0 @@
-import React, { useState } from 'react';
-import requests from 'requests';
-
-export default function useRequests() {
-  const [loading, setLoading] = useState(false);
-
-  const handleRequest = async () => {};
-}
