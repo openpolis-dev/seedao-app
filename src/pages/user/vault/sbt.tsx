6e4657c637de3c6607c81f7c70fc14ce053bd02d
CatFlow <22841916+Meow711@users.noreply.github.com>
2023-08-13 02:06:27 +0800

    handle get SBT

--- src/pages/user/vault/sbt.tsx
+++ src/pages/user/vault/sbt.tsx
@@ -1,38 +1,118 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { ethers } from 'ethers';
+import axios from 'axios';
+import { useWeb3React } from '@web3-react/core';
+
+const getNftsByContract = (account: string, contract: string, chain: number) => {
+  let base = '';
+  switch (chain) {
+    case 1:
+      base = 'https://api.nftscan.com';
+      break;
+    case 137:
+      base = 'https://polygonapi.nftscan.com';
+      break;
+  }
+  return axios.get(
+    `${base}/api/v2/account/own/${account}?erc_type=erc1155&show_attribute=false&sort_field=&sort_direction=&contract_address=${contract}`,
+    {
+      headers: {
+        'X-API-KEY': process.env.NEXT_PUBLIC_NFTSCAN_KEY,
+      },
+    },
+  );
+};
+
+type SBTtype = {
+  name: string;
+  image: string;
+};
 
 export default function SBTCard() {
-  const [ethereumProvider, setEthereumProvider] = useState<any>(null);
+  const { account } = useWeb3React();
   const [polygonProvider, setPolygonProvider] = useState<any>(null);
 
+  const [govSBTs, setGovSBTs] = useState<SBTtype[]>([]);
+  const [newSBTs, setNewSBTs] = useState<SBTtype[]>([]);
+
   useEffect(() => {
-    const _ethereum_provider = new ethers.providers.StaticJsonRpcProvider(
-      'https://eth-mainnet.g.alchemy.com/v2/YuNeXto27ejHnOIGOwxl2N_cHCfyLyLE',
-    );
-    setEthereumProvider(_ethereum_provider);
     const _polygon_provider = new ethers.providers.StaticJsonRpcProvider('https://poly-rpc.gateway.pokt.network');
     setPolygonProvider(_polygon_provider);
   }, []);
 
   useEffect(() => {
     // TODO
-  }, [ethereumProvider]);
-  useEffect(() => {
-    // TODO
+    // msc
+    const getMSC = async () => {
+      const contract = new ethers.Contract(
+        '0x2C436d61C5Af62bcbfeE40B1f0BE5B483DfA0E11',
+        [
+          {
+            inputs: [
+              {
+                internalType: 'address',
+                name: 'account',
+                type: 'address',
+              },
+              {
+                internalType: 'uint256',
+                name: 'id',
+                type: 'uint256',
+              },
+            ],
+            name: 'balanceOf',
+            outputs: [
+              {
+                internalType: 'uint256',
+                name: '',
+                type: 'uint256',
+              },
+            ],
+            stateMutability: 'view',
+            type: 'function',
+          },
+        ],
+        polygonProvider,
+      );
+    };
+    // getMSC();
   }, [polygonProvider]);
+
+  useEffect(() => {
+    const getSBTs = async (account: string) => {
+      // Governance Node
+      getNftsByContract(account, '0x9d34D407D8586478b3e4c39BE633ED3D7be1c80c', 1)
+        .then((res) => {
+          setGovSBTs(res.data.content.map((item: any) => ({ name: item.name, image: item.image_uri })));
+        })
+        .catch((err) => {
+          console.error('[SBT] gov failed', err);
+        });
+      // new SBT
+      getNftsByContract(account, '0x2C436d61C5Af62bcbfeE40B1f0BE5B483DfA0E11', 137)
+        .then((res) => {
+          setNewSBTs(
+            res.data.content
+              .filter((c: any) => !!c.image_uri)
+              .map((item: any) => ({ name: item.name, image: item.image_uri })),
+          );
+        })
+        .catch((err) => {
+          console.error('[SBT] new failed', err);
+        });
+    };
+    // account && getSBTs(account);
+  }, [account]);
 
   return (
     <Box>
       <div className="title">SBT</div>
       <SBTCardBox>
         <SBTList>
-          {[...Array(5)].map((item, index) => (
+          {[...govSBTs, ...newSBTs].map((item, index) => (
             <li key={index}>
-              {/*<img*/}
-              {/*  src="https://seedao-os-superapp.s3.ap-northeast-2.amazonaws.com/user_avatars/0x183f09c3ce99c02118c570e03808476b22d63191.jpg"*/}
-              {/*  alt=""*/}
-              {/*/>*/}
+              <img src={item.image} alt={item.name} />
             </li>
           ))}
         </SBTList>
