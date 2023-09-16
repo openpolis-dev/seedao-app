5b6146468b89fba660c6fb92f792a3b28a86dd3e
sr <7518647+wendychaung@users.noreply.github.com>
2023-09-16 13:54:55 +0800

    Update menu.tsx

--- src/layout/menu.tsx
+++ src/layout/menu.tsx
@@ -34,30 +34,6 @@ const LftLi = styled.div<{ selected?: boolean }>`
   }
   ${(props) => props.selected && 'color: var(--bs-primary);'}
 `;
-//
-// const Drawer = styled(MuiDrawer)(({ theme, open }) => ({
-//   '& .MuiDrawer-paper': {
-//     position: 'relative',
-//     whiteSpace: 'nowrap',
-//     width: drawerWidth,
-//     transition: theme.transitions.create('width', {
-//       easing: theme.transitions.easing.sharp,
-//       duration: theme.transitions.duration.enteringScreen,
-//     }),
-//     boxSizing: 'border-box',
-//     ...(!open && {
-//       overflowX: 'hidden',
-//       transition: theme.transitions.create('width', {
-//         easing: theme.transitions.easing.sharp,
-//         duration: theme.transitions.duration.leavingScreen,
-//       }),
-//       width: theme.spacing(7),
-//       [theme.breakpoints.up('sm')]: {
-//         width: theme.spacing(9),
-//       },
-//     }),
-//   },
-// }));
 
 type MenuItemType = {
   title: string;
