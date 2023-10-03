70543c5e9a380406ed201e9b16ab60d6e903a2b3
CatFlow <22841916+Meow711@users.noreply.github.com>
2023-10-03 22:32:13 +0800

    add pwa-install component

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import './assets/styles/font.css';
 import './assets/styles/custom.scss';
 import RouterLink from './router';
 import GlobalStyle from 'assets/styles/global';
+import InstallCheck from 'components/installPWA';
 
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -14,6 +15,7 @@ function App() {
       <Web3Provider>
         <GlobalStyle />
         <RouterLink />
+        <InstallCheck />
       </Web3Provider>
     </AuthProvider>
   );
