652b878ebffcce07fa353d00b59967387512e7e3
CatFlow <22841916+Meow711@users.noreply.github.com>
2023-10-04 12:07:11 +0800

    update sw

--- public/sw.js
+++ public/sw.js
@@ -1,52 +1,80 @@
+const MESSAGE_TYPE = {
+  PROJECT_ADD: 'proj_staff_add',
+  PROJECT_REMOVE: 'proj_staff_remove',
+  GUILD_ADD: 'guid_staff_add',
+  GUILD_REMOVE: 'guild_staff_remove',
+  ASSET_NEW: 'receive_assert',
+};
 // install
-self.addEventListener("install", (event) => {
-  console.log("installing…");
+self.addEventListener('install', (event) => {
+  console.log('installing…');
 });
 
 // activate
-self.addEventListener("activate", (event) => {
-  console.log("now ready to handle fetches!");
+self.addEventListener('activate', (event) => {
+  console.log('now ready to handle fetches!');
 });
 
 // fetch
-self.addEventListener("fetch", (event) => {
-  console.log("now fetch!");
+self.addEventListener('fetch', (event) => {
+  console.log('now fetch!');
 });
 
-self.addEventListener("push", function (event) {
+self.addEventListener('push', async function (event) {
   // Retrieve the textual payload from event.data (a PushMessageData object).
   // Other formats are supported (ArrayBuffer, Blob, JSON), check out the documentation
   // on https://developer.mozilla.org/en-US/docs/Web/API/PushMessageData.
-  const payload = event.data ? event.data.text() : "no payload";
-  const data = JSON.parse(payload);
-  // Keep the service worker alive until the notification is created.
+  console.log('event: ', event);
+
+  const json_string = event.data ? event.data.text() : '{}';
+  const payload = JSON.parse(json_string);
+
+  const { data, type } = payload.payload;
+  let route_path = '';
+  switch (type) {
+    case MESSAGE_TYPE.PROJECT_ADD:
+    case MESSAGE_TYPE.PROJECT_REMOVE:
+      route_path = `/project/${data.project_id}`;
+      break;
+    case MESSAGE_TYPE.GUILD_ADD:
+    case MESSAGE_TYPE.GUILD_REMOVE:
+      route_path = `/guild/${data.project_id}`;
+      break;
+    case MESSAGE_TYPE.ASSET_NEW:
+      route_path = 'user/vault';
+      break;
+    default:
+      break;
+  }
+
   event.waitUntil(
-    self.registration.showNotification(data.title || "notifycation title", {
-      ...data.data,
-    })
+    self.registration.showNotification(payload.title || 'notifycation title', {
+      body: payload.body,
+      data: route_path,
+    }),
   );
+
+  // const payload = event.data ? event.data.text() : "no payload";
+  // const data = JSON.parse(payload);
+  // // Keep the service worker alive until the notification is created.
+  // event.waitUntil(
+  //   self.registration.showNotification(data.title || "notifycation title", {
+  //     ...data.data,
+  //   })
+  // );
 });
 
-self.addEventListener("notificationclick", async function (event) {
-  console.log("click event:", event);
-  console.log("notificationt:", event.notification);
+self.addEventListener('notificationclick', async function (event) {
+  console.log('click event:', event);
+  console.log('notificationt:', event.notification);
   if (event.notification.data) {
-    // const client_arr = await clients.matchAll({
-    //   type: "window",
-    // });
-    // console.log("client_arr:", client_arr);
-    // if (client_arr.length > 0) {
-    //   client_arr[0].postMessage("New chat messages!");
-    // } else {
-    //   clients.openWindow(event.notification.data);
-    // }
-     const promise = new Promise(function (resolve) {
-       setTimeout(resolve, 1000);
-     }).then(function () {
-       // return the promise returned by openWindow, just in case.
-       // Opening any origin only works in Chrome 43+.
-       return clients.openWindow(event.notification.data);
-     });
+    const promise = new Promise(function (resolve) {
+      setTimeout(resolve, 1000);
+    }).then(function () {
+      // return the promise returned by openWindow, just in case.
+      // Opening any origin only works in Chrome 43+.
+      return clients.openWindow(event.notification.data);
+    });
     event.waitUntil(promise);
   }
 });
