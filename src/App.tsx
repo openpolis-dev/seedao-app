ef3f43df6b9f9fc05951a0872b767ff4d19e8f80
CatFlow <22841916+Meow711@users.noreply.github.com>
2023-10-06 13:13:07 +0800

    handle redirect

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,22 @@ import GlobalStyle from 'assets/styles/global';
 // import InstallCheck from 'components/installPWA';
 
 // import 'bootstrap/dist/css/bootstrap.min.css';
+import useMedia from 'hooks/useMedia';
+import { isPad } from 'utils/userAgent';
+import { useEffect } from 'react';
 
 function App() {
+  const isLargeScreen = useMedia('(min-width: 768px)', true);
+
+  useEffect(() => {
+    console.log('[isLargeScreen]', isLargeScreen);
+    console.log('[isPad]', isPad);
+    if (!isPad && !isLargeScreen) {
+      const mobile_app = process.env.REACT_APP_MOBILE_URL;
+      mobile_app && window.location.replace('https://dev-m.seedao.tech/');
+    }
+  }, [isPad, isLargeScreen]);
+
   return (
     <AuthProvider>
       <Web3Provider>
