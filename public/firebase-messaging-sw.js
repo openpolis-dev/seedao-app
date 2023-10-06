18e23172850bf407181a7fa36d4ab2562307285a
CatFlow <22841916+Meow711@users.noreply.github.com>
2023-10-06 20:16:26 +0800

    update push

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -14,19 +14,63 @@ const firebaseConfig = {
   measurementId: 'G-N4B2RPB1GJ',
 };
 
-
 firebase.initializeApp(firebaseConfig);
 
 // Retrieve firebase messaging
 const messaging = firebase.messaging();
 
+const MESSAGE_TYPE = {
+  PROJECT_ADD: 'proj_staff_add',
+  PROJECT_REMOVE: 'proj_staff_remove',
+  GUILD_ADD: 'guid_staff_add',
+  GUILD_REMOVE: 'guild_staff_remove',
+  ASSET_NEW: 'receive_assert',
+};
+
 messaging.onBackgroundMessage(function (payload) {
   console.log('Received background message ', payload);
+
+  const { type, data } = payload.data;
+
+  let route_path = '';
+  switch (type) {
+    case MESSAGE_TYPE.PROJECT_ADD:
+    case MESSAGE_TYPE.PROJECT_REMOVE:
+      route_path = `/project/${data.project_id}`;
+      break;
+    case MESSAGE_TYPE.GUILD_ADD:
+    case MESSAGE_TYPE.GUILD_REMOVE:
+      route_path = `/guild/${data.project_id}`;
+      break;
+    case MESSAGE_TYPE.ASSET_NEW:
+      route_path = '/user/vault';
+      break;
+    default:
+      break;
+  }
+  console.log('payload: ', payload);
+  console.log('route_path: ', route_path);
 
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
+    data: route_path,
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
+});
+
+self.addEventListener('notificationclick', async function (event) {
+  console.log('click event:', event);
+  console.log('notificationt:', event.notification);
+  if (event.notification.data) {
+    const promise = new Promise(function (resolve) {
+      setTimeout(resolve, 1000);
+    }).then(function () {
+      // return the promise returned by openWindow, just in case.
+      // Opening any origin only works in Chrome 43+.
+      return clients.openWindow(event.notification.data);
+    });
+    event.waitUntil(promise);
+  }
 });
