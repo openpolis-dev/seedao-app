f80f0174d0c65f1afe3605b07459c6ce10f31fc3
CatFlow <22841916+Meow711@users.noreply.github.com>
2023-10-07 10:39:46 +0800

    fix device check

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,19 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './i18n/i18n';
 import * as serviceWorkerRegistration from 'utils/serviceWorkerRegistration';
+import { isMobile, isPhone } from 'utils/userAgent';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(<App />);
+const isLargeScreen = window.innerWidth >= 768;
+if (isPhone || (isMobile && !isLargeScreen)) {
+  const mobile_app = process.env.REACT_APP_MOBILE_URL;
+  mobile_app && window.location.replace(mobile_app);
+} else {
+  const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+  root.render(<App />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
-serviceWorkerRegistration.register();
+  // If you want to start measuring performance in your app, pass a function
+  // to log results (for example: reportWebVitals(console.log))
+  // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+  reportWebVitals();
+  serviceWorkerRegistration.register();
+}
