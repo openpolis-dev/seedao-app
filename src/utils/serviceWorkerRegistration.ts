5cfd479eadf841e0f16438a29cf88c732cbe7b7a
CatFlow <22841916+Meow711@users.noreply.github.com>
2023-10-08 15:08:42 +0800

    upgrade the sw

--- src/utils/serviceWorkerRegistration.ts
+++ src/utils/serviceWorkerRegistration.ts
@@ -15,7 +15,7 @@ const firebaseConfig = {
 
 let messaging: Messaging;
 
-const version = 'v1';
+const version = 'v2';
 
 export function register(config?: any) {
   if ('serviceWorker' in navigator) {
