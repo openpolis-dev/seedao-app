7f5998e565e51105b5df8dd25acbbcbcd26e54eb
sr <7518647+wendychaung@users.noreply.github.com>
2023-10-08 16:50:36 +0800

    update calendar

--- src/pages/calendar.tsx
+++ src/pages/calendar.tsx
@@ -18,17 +18,6 @@ export default function CalendarPage() {
       {/*{loading && <LoadingBox />}*/}
 
       <CalendarBox />
-
-      {/*<iframe*/}
-      {/*  title="SeeDAO calendar"*/}
-      {/*  src={`https://calendar.google.com/calendar/embed?hl=${*/}
-      {/*    language || 'en'*/}
-      {/*  }&height=600&wkst=1&bgcolor=%23ffffff&ctz=Asia%2FShanghai&showTz=1&showPrint=0&showNav=1&showTitle=0&showDate=1&src=YzcwNGNlNTA5ODUxMmIwYjBkNzA3MjJlNjQzMGFmNDIyMWUzYzllYmM2ZDFlNzJhYTcwYjgyYzgwYmI2OTk5ZkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23E4C441`}*/}
-      {/*  style={{ border: 'none' }}*/}
-      {/*  width="100%"*/}
-      {/*  height={600}*/}
-      {/*  onLoad={handleIframeLoad}*/}
-      {/*></iframe>*/}
     </PageStyle>
   );
 }
