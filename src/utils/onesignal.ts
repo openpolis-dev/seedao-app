773ac9936d7a94df5e4d12438ef1f7204f11e32a
CatFlow <22841916+Meow711@users.noreply.github.com>
2023-10-31 10:47:12 +0800

    add version to onesignal

--- src/utils/onesignal.ts
+++ src/utils/onesignal.ts
@@ -1,11 +1,14 @@
 import OneSignal from 'react-onesignal';
 import getConfig from './envCofnig';
 
+const VERSION = '';
+
 export default function runOneSignal() {
   const app_id = getConfig().REAT_APP_ONESIGNAL_ID;
   console.log('app_id:', app_id);
   try {
-    OneSignal.init({ appId: app_id });
+    const _version_query = VERSION ? `?v=${VERSION}` : '';
+    OneSignal.init({ appId: app_id, serviceWorkerPath: `/OneSignalSDKWorker.js${_version_query}` });
     OneSignal.Slidedown.promptPush();
   } catch (error) {
     console.error('init onesignal error:', error);
