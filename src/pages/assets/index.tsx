86056f38ff04fff916f26429aa2711d6dde2e5ca
CatFlow <22841916+Meow711@users.noreply.github.com>
2023-11-07 01:42:51 +0800

    replace the icon in vault

--- src/pages/assets/index.tsx
+++ src/pages/assets/index.tsx
@@ -18,14 +18,10 @@ import { Button } from 'react-bootstrap';
 import { ChevronDown, ChevronUp, Pencil } from 'react-bootstrap-icons';
 import { ContainerPadding } from 'assets/styles/global';
 import { Link } from 'react-router-dom';
-import BalanceIconLight from 'assets/Imgs/light/vault/balance.svg';
-import WalletIconLight from 'assets/Imgs/light/vault/wallet.svg';
-import ChainIconLight from 'assets/Imgs/light/vault/chain.svg';
-import SignerIconLight from 'assets/Imgs/light/vault/signer.svg';
-import BalanceIconDark from 'assets/Imgs/dark/vault/balance.svg';
-import WalletIconDark from 'assets/Imgs/dark/vault/wallet.svg';
-import ChainIconDark from 'assets/Imgs/dark/vault/chain.svg';
-import SignerIconDark from 'assets/Imgs/dark/vault/signer.svg';
+import BalanceIcon from 'assets/Imgs/vault/balance.png';
+import WalletIcon from 'assets/Imgs/vault/wallet.png';
+import ChainIcon from 'assets/Imgs/vault/chain.png';
+import SignerIcon from 'assets/Imgs/vault/signer.png';
 import CopyIconSVG from 'components/svgs/copy';
 import ShareIconSVG from 'components/svgs/share';
 import ArrowIconSVG from 'components/svgs/downArrow';
@@ -330,7 +326,7 @@ export default function Index() {
             <div className="vaultInner">
               <InfoItem className="left">
                 <div>
-                  <IconStyle src={theme ? BalanceIconDark : BalanceIconLight} alt="" />
+                  <IconStyle src={BalanceIcon} alt="" />
                 </div>
                 <div className="info-right">
                   <div className="title">{t('Assets.TotalBalance')}</div>
@@ -340,7 +336,7 @@ export default function Index() {
               <div className="right">
                 <InfoItem>
                   <div>
-                    <IconStyle src={theme ? WalletIconDark : WalletIconLight} alt="" />
+                    <IconStyle src={WalletIcon} alt="" />
                   </div>
                   <div className="info-right">
                     <div className="title">{t('Assets.Wallet')}</div>
@@ -349,7 +345,7 @@ export default function Index() {
                 </InfoItem>
                 <InfoItem>
                   <div>
-                    <IconStyle src={theme ? SignerIconDark : SignerIconLight} alt="" />
+                    <IconStyle src={SignerIcon} alt="" />
                   </div>
                   <div className="info-right">
                     <div className="title">{t('Assets.MultiSign')}</div>
@@ -358,7 +354,7 @@ export default function Index() {
                 </InfoItem>
                 <InfoItem>
                   <div>
-                    <IconStyle src={theme ? ChainIconDark : ChainIconLight} alt="" />
+                    <IconStyle src={ChainIcon} alt="" />
                   </div>
                   <div className="info-right">
                     <div className="title">{t('Assets.Chain')}</div>
