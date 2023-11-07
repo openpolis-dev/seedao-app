004b4d9f5a0036bf418cff7424d7e22479954322
CatFlow <22841916+Meow711@users.noreply.github.com>
2023-11-07 16:40:34 +0800

    update member card

--- src/components/common/memberCard.tsx
+++ src/components/common/memberCard.tsx
@@ -3,6 +3,10 @@ import { IUser, UserRole } from 'type/user.type';
 import DefaultAvatar from 'assets/Imgs/defaultAvatar.png';
 import { useTranslation } from 'react-i18next';
 import PublicJs from 'utils/publicJs';
+import SocialIconBox from './socialIcon';
+import { useMemo } from 'react';
+import CopyBox from 'components/copy';
+import CopyIconSVG from 'components/svgs/copy';
 
 interface IProps {
   user: IUser;
@@ -12,15 +16,35 @@ interface IProps {
 
 export default function MemberCard({ user, sns, role }: IProps) {
   const { t } = useTranslation();
+  const snsDisplay = useMemo(() => {
+    return sns || PublicJs.AddressToShow(user.wallet || '', 6);
+  }, [sns, user]);
   return (
     <InnerBox>
       <ImgBox>
         <img className="avatar" src={user.avatar || DefaultAvatar} alt="" />
       </ImgBox>
       <div>
-        <div className="snsBox">{sns || PublicJs.AddressToShow(user.wallet || '')}</div>
+        <div className="sns-box">{snsDisplay}</div>
         {UserRole.Admin === role && <RoleTag>{t('Project.Moderator')}</RoleTag>}
       </div>
+      <HoverCard className="hover-card">
+        <HoverCardAvatar>
+          <img src={user.avatar || DefaultAvatar} alt="" />
+        </HoverCardAvatar>
+        <HoverNameBox>
+          <div className="sns-display">
+            <span className="sns">{snsDisplay}</span>
+            <CopyBox text={user.wallet || ''} dir="left">
+              <CopyIconSVG />
+            </CopyBox>
+          </div>
+          <div className="name">{user.name || t('My.DefaultName')}</div>
+        </HoverNameBox>
+        <SocialBox>
+          <SocialIconBox user={user} />
+        </SocialBox>
+      </HoverCard>
     </InnerBox>
   );
 }
@@ -31,9 +55,15 @@ const InnerBox = styled.div`
   justify-content: flex-start;
   margin-bottom: 26px;
   position: relative;
-  .snsBox {
+  .hover-card {
+    display: none;
+  }
+  &:hover .hover-card {
+    display: block;
+  }
+  .sns-box {
     color: var(--bs-body-color_active);
-    font-size: 14px;
+    font-size: 16px;
     font-family: Poppins-SemiBold;
     font-weight: 600;
     line-height: 18px;
@@ -43,6 +73,7 @@ const InnerBox = styled.div`
 
 const ImgBox = styled.div`
   margin-right: 12px;
+  position: relative;
   img {
     width: 44px;
     height: 44px;
@@ -60,4 +91,57 @@ const RoleTag = styled.span`
   padding: 0 8px;
   color: #000;
   font-size: 12px;
+`;
+
+const HoverCard = styled.div`
+  width: 292px;
+  position: absolute;
+  padding: 32px;
+  background: var(--bs-background);
+  border-radius: 16px 16px 16px 16px;
+  opacity: 1;
+  border: 1px solid var(--option-button-border-color);
+  left: 84px;
+  bottom: -50px;
+  z-index: 9;
+`;
+
+const HoverCardAvatar = styled.div`
+  img {
+    width: 80px;
+    height: 80px;
+    border-radius: 50%;
+  }
+`;
+
+const HoverNameBox = styled.div`
+  margin-top: 16px;
+  margin-bottom: 4px;
+  .sns-display {
+    display: flex;
+    align-items: center;
+    gap: 6px;
+  }
+  .sns {
+    font-size: 18px;
+    font-family: Poppins-SemiBold, Poppins;
+    font-weight: 600;
+    color: var(--bs-body-color_active);
+    line-height: 23px;
+  }
+  .nam {
+    margin-top: 8px;
+    font-size: 14px;
+    color: var(--bs-body-color_active);
+  }
+`;
+
+const SocialBox = styled.div`
+  .title {
+    font-size: 12px;
+    font-family: Poppins-SemiBold, Poppins;
+    font-weight: 600;
+    color: var(--bs-body-color);
+    margin-bottom: 14px;
+  }
 `;
