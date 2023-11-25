319618fe5794a2932dfcecf74d836abcd477ce9e
CatFlow <22841916+Meow711@users.noreply.github.com>
2023-11-25 16:03:39 +0800

    sns: add sns provider

--- src/pages/sns/register.tsx
+++ src/pages/sns/register.tsx
@@ -1,11 +1,65 @@
 import RegisterSNSStep1 from './step1';
 import RegisterSNSStep2 from './step2';
 
-export default function RegisterSNS() {
+import React, { useReducer, createContext, useContext } from 'react';
+
+enum AppActionType {}
+
+interface IState {
+  step: number;
+}
+interface IAction {
+  type: AppActionType;
+  payload: any;
+}
+
+const INIT_STATE: IState = { step: 1 };
+
+export const ACTIONS = {
+  SET_STEP: 'set_step',
+};
+
+const SNSContext = createContext<{
+  state: IState;
+  dispatch: React.Dispatch<IAction>;
+}>({
+  state: INIT_STATE,
+  dispatch: () => null,
+});
+
+const reducer = (state: IState, action: IAction): IState => {
+  //   TODO
+  switch (action.type) {
+    // TODO
+    default:
+      throw new Error(`Unknown type: ${action.type}`);
+  }
+};
+
+const SNSProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, INIT_STATE);
+  return <SNSContext.Provider value={{ state, dispatch }}>{children}</SNSContext.Provider>;
+};
+
+const useSNSContext = () => ({ ...useContext(SNSContext) });
+
+const RegisterSNSWrapper = () => {
+  const {
+    state: { step },
+  } = useSNSContext();
+  console.log('step', step);
   return (
-    <div>
+    <>
       <RegisterSNSStep1 />
       <RegisterSNSStep2 />
-    </div>
+    </>
+  );
+};
+
+export default function RegisterSNS() {
+  return (
+    <SNSProvider>
+      <RegisterSNSWrapper />
+    </SNSProvider>
   );
 }
