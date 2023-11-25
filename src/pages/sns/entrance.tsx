0274bf68b799e93d70af1a3dc3f4f9aba4fb8812
CatFlow <22841916+Meow711@users.noreply.github.com>
2023-11-25 21:43:30 +0800

    sns: modified entrace root style

--- src/pages/sns/entrance.tsx
+++ src/pages/sns/entrance.tsx
@@ -5,7 +5,12 @@ import LogoImg from '../../assets/Imgs/sns.svg';
 
 const Box = styled.div`
   position: relative;
-  height: 100%;
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  left: 0;
+  top: 0;
+  z-index: 99;
 `;
 const TopBox = styled.div`
   width: 100%;
