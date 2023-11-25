c1ec6c90065e2b355b0344b581d567e05a5b494c
wendychaung <7518647+wendychaung@users.noreply.github.com>
2023-11-25 23:18:21 +0800

    update profile

--- src/components/profile/seed.tsx
+++ src/components/profile/seed.tsx
@@ -1,22 +1,46 @@
 import styled from 'styled-components';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper/modules';
+import RhtArr from '../../assets/Imgs/profile/right.svg';
 
 import 'swiper/css';
+import 'swiper/css/navigation';
 
 const Box = styled.div`
   width: 100%;
   min-height: 10px;
   display: flex;
+  align-items: center;
+  position: relative;
   .mySwiper {
   }
   .swiper-slide {
     display: flex;
     width: 88px;
   }
+
+  .lft {
+    position: relative;
+    margin-right: 20px;
+    background: url(${RhtArr}) no-repeat;
+    transform: rotate(180deg);
+    &:after {
+      content: '';
+    }
+  }
+  .rht {
+    position: relative;
+    margin-left: 20px;
+    margin-top: 15px;
+    background: url(${RhtArr}) no-repeat;
+    &:after {
+      content: '';
+    }
+  }
 `;
 const OuterBox = styled.div`
-  width: calc(100vw - 370px);
+  width: calc(100vw - 440px);
 `;
 
 const LiBox = styled.div`
@@ -35,24 +59,41 @@ const LiBox = styled.div`
     padding: 9px 0 0;
   }
 `;
-export default function SeedList() {
+
+interface Iprops {
+  list: any[];
+}
+
+export default function SeedList(props: Iprops) {
+  const { list } = props;
+
   return (
     <Box>
+      <div className={`swiper-button-prev lft swiper-button-prev_nft`}></div>
+
       <OuterBox>
-        <Swiper className="mySwiper" slidesPerView="auto" spaceBetween={20}>
-          {[...Array(20)].map((item, index) => (
-            <SwiperSlide key={index}>
-              <LiBox>
-                <img
-                  src="https://img2.baidu.com/it/u=3012806272,1276873993&fm=253&fmt=auto&app=138&f=JPEG?w=800&h=500"
-                  alt=""
-                />
-                <span>ID 3413</span>
-              </LiBox>
-            </SwiperSlide>
-          ))}
+        <Swiper
+          className="mySwiper"
+          slidesPerView="auto"
+          navigation={{
+            nextEl: `.swiper-button-next_nft`,
+            prevEl: `.swiper-button-prev_nft`,
+          }}
+          modules={[Navigation]}
+          spaceBetween={20}
+        >
+          {!!list?.length &&
+            list.map((item: any, index: number) => (
+              <SwiperSlide key={index}>
+                <LiBox>
+                  <img src={item.url} alt="" />
+                  <span>ID {item.token_id}</span>
+                </LiBox>
+              </SwiperSlide>
+            ))}
         </Swiper>
       </OuterBox>
+      <div className={`swiper-button-next rht swiper-button-next_nft`}></div>
     </Box>
   );
 }
