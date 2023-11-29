ae5a6ac04d02a4f99b6c63acb50cfbc0ffa777b4
CatFlow <22841916+Meow711@users.noreply.github.com>
2023-11-29 20:29:18 +0800

    sns: fix switch

--- src/pages/sns/switchModal.tsx
+++ src/pages/sns/switchModal.tsx
@@ -7,6 +7,15 @@ import { AppActionType, useAuthContext } from 'providers/authProvider';
 import { ethers } from 'ethers';
 import { builtin } from '@seedao/sns-js';
 import ABI from 'assets/abi/snsRegister.json';
+import { SELECT_WALLET } from 'utils/constant';
+import { Wallet } from '../../wallet/wallet';
+import { sendTransaction } from '@joyid/evm';
+import useToast, { ToastType } from 'hooks/useToast';
+
+const buildSwitchData = (sns: string) => {
+  const iface = new ethers.utils.Interface(ABI);
+  return iface.encodeFunctionData('setDefaultAddr', [sns.replace('.seedao', ''), builtin.PUBLIC_RESOLVER_ADDR]);
+};
 
 interface IProps {
   select: string;
@@ -20,17 +29,50 @@ export default function SwitchModal({ select, handleClose }: IProps) {
     dispatch,
   } = useAuthContext();
 
+  const { showToast } = useToast();
+
+  const handleCheckTx = (hash: string) => {
+    let timer: any;
+    const timerFunc = async () => {
+      console.log('check tx hash:', hash);
+      provider.getTransactionReceipt(hash).then((r: any) => {
+        if (r && r.status === 1) {
+          handleClose(select);
+          dispatch({ type: AppActionType.SET_SNS, payload: select });
+          dispatch({ type: AppActionType.SET_LOADING, payload: false });
+        } else if (r && r.status === 2) {
+          dispatch({ type: AppActionType.SET_LOADING, payload: false });
+          showToast('transaction failed', ToastType.Danger);
+        }
+      });
+    };
+    timer = setInterval(timerFunc, 1500);
+  };
+
   const handleSwitch = async () => {
     dispatch({ type: AppActionType.SET_LOADING, payload: true });
     try {
-      const contract = new ethers.Contract(builtin.SEEDAO_REGISTRAR_CONTROLLER_ADDR, ABI, provider.getSigner());
-      const tx = await contract.setDefaultAddr(select.replace('.seedao', ''), builtin.PUBLIC_RESOLVER_ADDR);
-      await tx.wait();
-      handleClose(select);
-      dispatch({ type: AppActionType.SET_SNS, payload: select });
+      const wallet = localStorage.getItem(SELECT_WALLET);
+      if (wallet && wallet === Wallet.JOYID_WEB) {
+        const txHash = await sendTransaction({
+          to: builtin.SEEDAO_REGISTRAR_CONTROLLER_ADDR,
+          from: account,
+          value: '0',
+          data: buildSwitchData(select),
+        });
+        console.log('joyid txHash:', txHash);
+        handleCheckTx(txHash);
+      } else {
+        const contract = new ethers.Contract(builtin.SEEDAO_REGISTRAR_CONTROLLER_ADDR, ABI, provider.getSigner());
+        const tx = await contract.setDefaultAddr(select.replace('.seedao', ''), builtin.PUBLIC_RESOLVER_ADDR);
+        console.log('tx:', tx);
+        await tx.wait();
+        handleClose(select);
+        dispatch({ type: AppActionType.SET_SNS, payload: select });
+        dispatch({ type: AppActionType.SET_LOADING, payload: false });
+      }
     } catch (error) {
       console.error(error);
-    } finally {
       dispatch({ type: AppActionType.SET_LOADING, payload: false });
     }
   };
