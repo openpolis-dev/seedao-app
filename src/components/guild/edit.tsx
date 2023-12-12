09b575ae839c1922f942aa5f07c7a7b0a65890d4
wendychaung <7518647+wendychaung@users.noreply.github.com>
2023-12-12 20:38:06 +0800

    update edit guild/project upload image

--- src/components/guild/edit.tsx
+++ src/components/guild/edit.tsx
@@ -304,6 +304,12 @@ const ImgBox = styled.div`
   align-items: center;
   justify-content: center;
   position: relative;
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    object-position: center;
+  }
   .upload {
     display: none;
   }
