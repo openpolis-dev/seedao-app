4faee75f5a4181121bc1e2fce76d1ea55e7c8a34
CatFlow <22841916+Meow711@users.noreply.github.com>
2023-12-23 15:34:57 +0800

    add error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ import 'md-editor-rt/lib/style.css';
 import { DeSchoolProvider, CourseContextProvider } from '@deschool-protocol/react';
 import '@deschool-protocol/react/dist/styles/index.css';
 // import CheckOnboarding from 'components/checkOnboarding';
+import ErrorBoundary from 'components/ErrorBoundary';
 
 const chains = getConfig().NETWORK.chainId === 1 ? [mainnet] : [goerli, mainnet];
 
@@ -36,34 +37,36 @@ function App() {
   const dtoken = new URLSearchParams(search).get('dtoken') || '';
 
   return (
-    <WagmiConfig config={wagmiConfig}>
-      <AuthProvider>
-        <GlobalStyle />
-        <DeSchoolProvider
-          config={{
-            baseUrl: 'https://deschool.app/goapiProduction',
-            token: dtoken,
-          }}
-        >
-          <CourseContextProvider>
-            <RouterLink />
-          </CourseContextProvider>
-          {/* <InstallCheck /> */}
-        </DeSchoolProvider>
-        {/* <CheckOnboarding /> */}
-        <Web3Modal
-          defaultChain={goerli}
-          projectId={projectId}
-          ethereumClient={ethereumClient}
-          explorerRecommendedWalletIds={[
-            'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96',
-            // '80c7742837ad9455049270303bccd55bae39a9e639b70d931191269d3a76320a',
-            // '4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0'
-          ]}
-          explorerExcludedWalletIds="ALL"
-        />
-      </AuthProvider>
-    </WagmiConfig>
+    <ErrorBoundary>
+      <WagmiConfig config={wagmiConfig}>
+        <AuthProvider>
+          <GlobalStyle />
+          <DeSchoolProvider
+            config={{
+              baseUrl: 'https://deschool.app/goapiProduction',
+              token: dtoken,
+            }}
+          >
+            <CourseContextProvider>
+              <RouterLink />
+            </CourseContextProvider>
+            {/* <InstallCheck /> */}
+          </DeSchoolProvider>
+          {/* <CheckOnboarding /> */}
+          <Web3Modal
+            defaultChain={goerli}
+            projectId={projectId}
+            ethereumClient={ethereumClient}
+            explorerRecommendedWalletIds={[
+              'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96',
+              // '80c7742837ad9455049270303bccd55bae39a9e639b70d931191269d3a76320a',
+              // '4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0'
+            ]}
+            explorerExcludedWalletIds="ALL"
+          />
+        </AuthProvider>
+      </WagmiConfig>
+    </ErrorBoundary>
   );
 }
 
