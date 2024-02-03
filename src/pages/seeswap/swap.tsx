b40c72c84ee3d21d072ba64a084b891c882fe3ca
CatFlow <22841916+Meow711@users.noreply.github.com>
2024-02-04 01:00:04 +0800

    add test swap

--- src/pages/seeswap/swap.tsx
+++ src/pages/seeswap/swap.tsx
@@ -4,24 +4,79 @@ import '@uniswap/widgets/fonts.css';
 import { useEthersSigner } from 'hooks/ethersNew';
 import { ContainerPadding } from 'assets/styles/global';
 import BackerNav from 'components/common/backNav';
+import { useEffect, useState } from 'react';
+import { useSwitchNetwork } from 'wagmi';
 
 const theme: Theme = {
   fontFamily: 'Poppins-Regular',
 };
 
+const CHAIN_ID = 5;
+
+const TOKEN_LIST = [
+  {
+    address: '0x9D56b227Bdd65232CC6400741e303c46067CC9F6',
+    name: 'MockUSDT',
+    symbol: 'MockUSDT',
+    decimals: 6,
+    chainId: 5,
+    logoURI:
+      'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x6B175474E89094C44Da98b954EedeAC495271d0F/logo.png',
+  },
+  {
+    address: '0xF0f214BE4Af4625F5B9EA8A3CE2cCf6d8f35F9f4',
+    name: 'TESTSEE',
+    symbol: 'TESTSEE',
+    decimals: 18,
+    chainId: 5,
+    logoURI:
+      'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x6B175474E89094C44Da98b954EedeAC495271d0F/logo.png',
+  },
+];
+
 export default function SeeSwap() {
-  const signer = useEthersSigner({ chainId: 137 });
+  const signer = useEthersSigner({ chainId: CHAIN_ID });
+  const { switchNetworkAsync } = useSwitchNetwork();
+
+  console.log('====signer', signer, signer?.provider);
+  const [chainId, setChainId] = useState<number>();
+
+  useEffect(() => {
+    const checkNetwork = async () => {
+      if (!signer) {
+        return;
+      }
+      try {
+        const { chainId: _chainId } = await signer?.provider.getNetwork();
+        console.log('====chainId', _chainId);
+        if (_chainId !== CHAIN_ID && switchNetworkAsync) {
+          await switchNetworkAsync(CHAIN_ID);
+        }
+        setChainId(CHAIN_ID);
+      } catch (error) {
+        if (switchNetworkAsync) {
+          await switchNetworkAsync(CHAIN_ID);
+          setChainId(CHAIN_ID);
+        }
+      }
+    };
+    checkNetwork();
+  }, [signer, switchNetworkAsync]);
+
   return (
     <Container>
       <BackerNav to="/home" title="SNS" />
       <SwapContainer>
-        <SwapWidget
-          provider={signer?.provider}
-          onConnectWalletClick={() => {
-            console.log('onConnectWalletClick');
-          }}
-          theme={theme}
-        />
+        {chainId === CHAIN_ID && (
+          <SwapWidget
+            tokenList={TOKEN_LIST}
+            // defaultInputAmount={10}
+            defaultInputTokenAddress={{ [CHAIN_ID]: '0x9D56b227Bdd65232CC6400741e303c46067CC9F6' }}
+            defaultOutputTokenAddress={{ [CHAIN_ID]: '0xF0f214BE4Af4625F5B9EA8A3CE2cCf6d8f35F9f4' }}
+            provider={signer?.provider}
+            theme={theme}
+          />
+        )}
       </SwapContainer>
     </Container>
   );
